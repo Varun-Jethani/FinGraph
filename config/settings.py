bb44ca8d541a6307bec9c117756749045fb1f6f6
Varun <varunjethani@outlook.com>
2024-04-03 14:15:24 +0530

    update Settings vercel

--- config/settings.py
+++ config/settings.py
@@ -117,18 +117,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+import os
 #manual
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join("static"),
 ]
-import os
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
