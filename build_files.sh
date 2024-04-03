e5824ab82f29812f590f50cab80df9d7dd89a8cf
Varun <varunjethani@outlook.com>
2024-04-03 16:46:23 +0530

    req update for vercel

--- build_files.sh
+++ build_files.sh
@@ -2,7 +2,7 @@
 echo "Build Start"
 python3.9 -m pip install --upgrade pip
 
-pip install -r requirements.txt
+pip install -r req2.txt
 echo "Requirements installed"
 python3.9 manage.py collectstatic --no-input --clear
 echo "Build End"
