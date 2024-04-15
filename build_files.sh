c31d838b460aefbd2d4a90ea368eef2a369a02ee
Varun <varunjethani@outlook.com>
2024-04-15 12:33:28 +0530

    update buildsh

--- build_files.sh
+++ build_files.sh
@@ -2,8 +2,8 @@
 echo "Build Start"
 python3.9 -m pip install --upgrade pip
 echo "apt start"
-sudo apt-get update
-sudo apt-get install gcc libmysqlclient-dev python3-dev
+apt-get update
+apt-get install gcc libmysqlclient-dev python3-dev
 echo "apt end"
 pip install -r requirements.txt
 echo "Requirements installed"
